60e36dc95289538dd1ffe469dc4650a67eb9675a
xbman <manxiaobooo@126.com>
2018-01-02 23:08:42 +0800

    update

--- src/app/routes/user-manager/farmers/farmers.component.ts
+++ src/app/routes/user-manager/farmers/farmers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd';
+import { NzMessageService,NzModalService } from 'ng-zorro-antd';
 import { getTimeDistance, yuan, fixedZero } from '@delon/abc';
 import { DCDataService } from '../../../services/data.service';
 import { DCAuthService } from '../../../services/auth.service';
@@ -8,18 +8,29 @@ import { isSameDay } from 'date-fns';
 
 @Component({
     selector: 'app-user-farmer',
-    templateUrl: './farmers.component.html',
-    styleUrls: ['./farmers.component.less']
+    templateUrl: './farmers.component.html'
 })
 export class UserFarmerManagerComponent implements OnInit, OnDestroy {
     q: any = {
-        status: 'all'
+        pi: 1,
+        ps: 5,
+        sorter: '',
+        userName: '',
+        updatedAt: '',
+        address: '',
+        phone: '',
+        status: 'validate',
+        role: ''
     };
+
+    sortMap: any = {};
     loading = false;
+    modalVisible3 = false;
     roles;
     farmerRole;
-    farmers;
-    constructor(public msg: NzMessageService,private dataservice:DCDataService,private auth:DCAuthService) {}
+    farmers = [];
+    currentFarmer;
+    constructor(public msg: NzMessageService,private modal:NzModalService,private dataservice:DCDataService,private auth:DCAuthService) {}
 
     ngOnInit() {
         this.roles = this.auth.roles;
@@ -30,6 +41,7 @@ export class UserFarmerManagerComponent implements OnInit, OnDestroy {
     }
 
     getFarmers(){
+        this.loading = true;
         this.dataservice.getUsersByRole(this.farmerRole.id).then((fs:any)=>{
             this.farmers = fs.json();
             this.farmers.forEach(f => {
@@ -39,8 +51,30 @@ export class UserFarmerManagerComponent implements OnInit, OnDestroy {
                     }
                 });
             });
+            this.loading = false;
             console.info(this.farmers);
         });
+    }
+
+    view(f){
+        this.currentFarmer = f;
+        this.modalVisible3 = true;
+    }
+
+    viewOk(){
+        this.modalVisible3 = false;
+    }
+
+    sort(field: string, value: any) {
+        this.farmers = _.orderBy(this.farmers, [field], [value == 'ascend' ? 'asc' : 'desc']);
+    }
+
+    dataChange(e){
+
+    }
+
+    pageChange(e){
+
     }
 
     ngOnDestroy(): void {
